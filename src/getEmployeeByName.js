5fcd20f212ca86f867916cef7417ee78ba371641
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-08 17:55:26 -0300

    REQ3 - Finalizado, função para encontrar pessoa pelo primeiro e ultimo nome.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,19 @@ const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  const { employees } = data;
+  const findEmployees = employees.find((element) => {
+    const { firstName, lastName } = element;
+    const verifyNames = firstName.includes(employeeName) || lastName.includes(employeeName);
+    return verifyNames;
+  });
+  return findEmployees;
 };
+
+console.log(getEmployeeByName('Nelson'));
 
 module.exports = getEmployeeByName;
