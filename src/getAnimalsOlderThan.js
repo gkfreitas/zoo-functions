6cc43285c955248f6098967b0bd98134b35b5d14
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-08 17:45:14 -0300

    REQ2 - Finalizado!

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  const findElement = species.find((element) => element.name === animal);
+  const { residents } = findElement;
+  const everyElement = residents.every((element) => element.age > age);
+  return everyElement;
 };
 
 module.exports = getAnimalsOlderThan;
