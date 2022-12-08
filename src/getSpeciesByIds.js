3c9427daa93972f562beb2b06d7593eaf80c07f9
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-08 17:16:28 -0300

    Exercício 1 finalizado.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const getSpeciesByIds = (...ids) => {
   // seu código aqui
+  const loopId = ids.map((id) => {
+    const { species } = data;
+    const filterId = species.find((element) => element.id === id);
+    return filterId;
+  });
+  return loopId;
 };
 
 module.exports = getSpeciesByIds;
