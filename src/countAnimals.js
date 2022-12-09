b1981b20d77b22327e9bb8937de4a30ab6de8db1
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-09 11:00:21 -0300

    REQ5 Finalizado, função para contar quantos animais existem no zoo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 const countAnimals = (animal) => {
-  // seu código aqui
+  const objectEveryAnimals = {};
+  species.forEach((element) => {
+    const animalFind = species.find((e) => e.name === element.name);
+    objectEveryAnimals[element.name] = animalFind.residents.length;
+  });
+  if (animal === undefined) {
+    return objectEveryAnimals;
+  }
+  const findAnimal = species.find((element) => {
+    const objectAnimal = element.name === animal.species;
+    return objectAnimal;
+  });
+  const animalCount = findAnimal.residents.length;
+  if (Object.keys(animal).length === 1) {
+    return animalCount;
+  }
+  const findAnimalSex = findAnimal.residents.filter((element) => element.sex === animal.sex);
+  return findAnimalSex.length;
 };
 
+console.log(countAnimals({ species: 'giraffes' }));
 module.exports = countAnimals;
