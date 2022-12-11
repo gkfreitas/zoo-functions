fcbfbcc6be7738e307d91afcb93a5d35dcf6f312
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-11 08:40:30 -0300

    REQ9 Finalizado, função para mostrar dias e horarios dos animais

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,17 +1,15 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
+const { species, hours } = data;
 
 // Filtra semanas que estão disponível.
-const weeks = species.map((element) => element.availability);
-const weeksArray = weeks.reduce((acc, cur) => acc.concat(cur));
-const weeksFilter = weeksArray.filter((element, index) => {
-  const takeRepeated = weeksArray.indexOf(element) === index;
-  return takeRepeated;
-});
+const weeks = Object.keys(hours);
+const weekHours = Object.values(hours);
+// Mostra todos os animais do zoo
+const animalNames = species.map((element) => element.name);
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+const verifyAnimals = (scheduleTarget) => {
+// Verifica quais dias vão aparecer os animais indicados.
   const findSpecies = species.find((element) => {
     const findTarget = element.name === scheduleTarget;
     return findTarget;
@@ -19,7 +17,43 @@ const getSchedule = (scheduleTarget) => {
   const arrayAvailability = findSpecies.availability;
   return arrayAvailability;
 };
-console.log(weeksFilter);
-console.log(getSchedule('lions'));
+
+const verifyAnimalsDays = (scheduleTarget) => {
+  const indexWeek = weeks.indexOf(scheduleTarget);
+  const message = weekHours[indexWeek];
+  const filterAvalability = species.filter((e) => e.availability.includes(scheduleTarget));
+  const animalsAvalability = filterAvalability.map((element) => element.name);
+  if (scheduleTarget === 'Monday') {
+    return {
+      [scheduleTarget]: {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      },
+    };
+  }
+  return {
+    [scheduleTarget]: {
+      officeHour: `Open from ${message.open}am until ${message.close}pm`,
+      exhibition: animalsAvalability,
+    },
+  };
+};
+
+const getSchedule = (scheduleTarget) => {
+// seu código aqui
+  if (animalNames.includes(scheduleTarget)) {
+    return verifyAnimals(scheduleTarget);
+  }
+  // Verifica quais animais vão aparecer nos dias indicados
+  if (weeks.includes(scheduleTarget)) {
+    return verifyAnimalsDays(scheduleTarget);
+  }
+  // Retorna todos os dias dias horários e animais que irão aparecer.
+  let allDays = {};
+  weeks.forEach((element) => {
+    allDays = Object.assign(allDays, verifyAnimalsDays(element));
+  }, {});
+  return allDays;
+};
 
 module.exports = getSchedule;
