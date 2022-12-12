f29c5b34fe94d09fa08361f4b40f098ae9045806
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-12 11:23:58 -0300

    REQ11 Finalizado, função para dar informações sobre funcionarios

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,70 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const { employees, species } = data;
+
+const verifyEmployeed = (object) => {
+  if (object === undefined) {
+    return undefined;
+  }
+  const arrayIds = employees.map((e) => e.id);
+  const arrayFirstNames = employees.map((e) => e.firstName);
+  const arrayLastNames = employees.map((e) => e.lastName);
+  const allArrays = arrayIds.concat(arrayFirstNames, arrayLastNames);
+  const objectValue = Object.values(object);
+  const checkTrueOrFalse = allArrays.some((e) => e === objectValue[0]);
+  return checkTrueOrFalse;
 };
 
+const findEmployeed = (object) => {
+  // Encontrar a pessoa pelo id, nome ou sobrenome;
+  const employeed = employees.find((idName) => {
+    const valueObject = Object.values(object);
+    const verifyIdName = Object.values(idName).includes(valueObject[0]);
+    return verifyIdName;
+  });
+  return employeed;
+};
+
+const employeedInfo = (object) => {
+  // Encontrar os animais que esta pessoa é responsável.
+  const findInfo = findEmployeed(object);
+  const animalResponsible = findInfo.responsibleFor;
+  const animalFiltered = animalResponsible.map((e) => species.filter((id) => id.id === e));
+  const animals = [];
+  const locations = [];
+  animalFiltered.forEach((e) => e.forEach((e2) => animals.push(e2.name)));
+  animalFiltered.forEach((e) => e.forEach((e2) => locations.push(e2.location)));
+  const objectInfos = {
+    id: findInfo.id,
+    fullName: `${findInfo.firstName} ${findInfo.lastName}`,
+    species: animals,
+    locations,
+  };
+  return objectInfos;
+};
+
+const mapId = employees.map((element) => element.id);
+const objectId = {};
+const getEmployeesCoverage = (object) => {
+  if (verifyEmployeed(object) === false) {
+    throw new Error('Informações inválidas');
+  }
+  if (verifyEmployeed(object)) {
+    try {
+      verifyEmployeed(object);
+      return employeedInfo(object);
+    } catch (error) {
+      return error.message;
+    }
+  }
+  if (verifyEmployeed(object) === undefined) {
+    const loopId = mapId.map((e) => {
+      objectId.id = e;
+      return employeedInfo(objectId);
+    });
+    return loopId;
+  }
+};
+
+console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
