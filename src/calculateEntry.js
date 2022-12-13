45cece89d6b9bda9ad9d5f9ff7ff529afb1967ec
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-13 18:08:27 -0300

    CalculateEntry melhorado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,33 +2,24 @@ const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
   // seu código aqui
-  const entrantsAge = entrants.map((element) => {
-    const arrayAge = element.age;
-    return arrayAge;
-  });
+  const entrantsAge = entrants.map((element) => element.age);
   const childs = entrantsAge.filter((age) => age < 18);
   const adult = entrantsAge.filter((age) => age >= 18 && age < 50);
   const senior = entrantsAge.filter((age) => age >= 50);
-  return {
-    child: childs.length,
-    adult: adult.length,
-    senior: senior.length,
-  };
+  return { child: childs.length, adult: adult.length, senior: senior.length };
 };
+
+const { prices } = data;
 
 const calculateEntry = (entrants) => {
   // seu código aqui
-  if (!Array.isArray(entrants)) {
-    return 0;
-  }
+  if (!Array.isArray(entrants)) return 0;
   // Soma dos valores
   const { child, adult, senior } = countEntrants(entrants);
-
-  const childCalc = child * 20.99;
-  const adultCalc = adult * 49.99;
-  const seniorCalc = senior * 24.99;
+  const childCalc = child * prices.child;
+  const adultCalc = adult * prices.adult;
+  const seniorCalc = senior * prices.senior;
   const calcTotal = childCalc + adultCalc + seniorCalc;
   return calcTotal;
 };
-
 module.exports = { calculateEntry, countEntrants };
