ab5116d9db03b9be25ab76c74990ced6f46313ef
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-12-13 15:17:36 -0300

    REQ14 Finalizado, função para filtrar pesquisa de animais

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
+const { species } = data;
+const animalLoc = { NE: '', NW: '', SE: '', SW: '' };
+const locations = Object.keys(animalLoc);
+
+const verifyOptions = (options) => {
+  let parameter = options;
+  if (options === undefined) parameter = {};
+  if (Object.keys(parameter).includes('includeNames')) {
+    return false;
+  }
+  return true;
 };
 
+const animalMapParameters = (options, e) => {
+  const filterLocation = species.filter(({ location }) => location === e);
+  animalLoc[e] = filterLocation.map(({ name, residents }) => {
+    const objKeys = Object.keys(options);
+    let resP = residents;
+    if (objKeys.includes('sex')) resP = residents.filter((sex) => sex.sex === options.sex);
+    const residentsMapName = resP.map((names) => names.name);
+    const newObj = { [name]: residentsMapName };
+    if (objKeys.includes('sorted')) residentsMapName.sort();
+    return newObj;
+  });
+};
+
+const getAnimalMap = (options) => {
+  // seu código aqui
+  locations.forEach((e) => {
+    const filterLocation = species.filter(({ location }) => location === e);
+    if (verifyOptions(options)) animalLoc[e] = filterLocation.map(({ name }) => name);
+    if (!verifyOptions(options)) {
+      animalMapParameters(options, e);
+    }
+  });
+  return animalLoc;
+};
+console.log(getAnimalMap({ includeNames: true, sorted: true, sex: 'male' }));
 module.exports = getAnimalMap;
